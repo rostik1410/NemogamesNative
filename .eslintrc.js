7dd9467b40fa60878725515fd5d5414b7544bcb3
Rostyslav Ptashynskyi <ptashinskij.rostik@gmail.com>
2019-09-26 18:58:51 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,58 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+	root: true,
+	parser: 'babel-eslint',
+	extends: ['react-app', 'prettier'],
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	plugins: ['react', 'prettier', 'json'],
+	rules: {
+		'no-unused-vars': [
+			'error',
+			{
+				args: 'after-used',
+				argsIgnorePattern: '^event$',
+				ignoreRestSiblings: true,
+				vars: 'all',
+			},
+		],
+		'default-case': 0, // tmp
+		'jsx-a11y/anchor-is-valid': 0, // tmp
+		'no-useless-escape': 0, // tmp
+		// curly: [2, 'multi-line'],
+		'jsx-quotes': 1,
+		'no-shadow': 1,
+		'no-trailing-spaces': 1,
+		'object-curly-spacing': [1, 'always'],
+		quotes: [2, 'single', 'avoid-escape'],
+		'react/jsx-boolean-value': 1,
+		'react/jsx-uses-react': 1,
+		'react/no-deprecated': 1,
+		'react/jsx-uses-vars': 1,
+		'react/jsx-wrap-multilines': 1,
+		// 'react/no-did-mount-set-state': 1,
+		// 'react/no-did-update-set-state': 1,
+		'react/no-unknown-property': 1,
+		'react/self-closing-comp': 1,
+		'react/sort-prop-types': 1,
+		semi: 2,
+		strict: 0,
+	},
+	globals: {
+		SOCKET_IP: true,
+		SERVER_NAME: true,
+		SERVER_NAME_DEV: true,
+		SERVER_NAME_LOCAL: true,
+		SERVER_NAME_BETA: true,
+		SENTRY_URL: true,
+		chrome: true,
+		location: true,
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			legacyDecorators: true,
+		},
+	},
 };
--- src/components/screens/StreamScreen.js
+++ src/components/screens/StreamScreen.js
@@ -8,48 +8,45 @@ import { getStream } from '../../actions/StreamsActions';
 import Wrapper from '../Wrapper';
 
 class StreamScreen extends Component {
-    static propTypes = {
-        dispatch: PropTypes.func.isRequired
-    }
+	static propTypes = {
+		dispatch: PropTypes.func.isRequired,
+	};
 
-    componentDidMount() {
-        this.props.dispatch(getStream(this.props.navigation.state.params.data));
-    }
+	componentDidMount() {
+		this.props.dispatch(getStream(this.props.navigation.state.params.data));
+	}
 
-    render() {
-        const { stream, error } = this.props;
-        
-        //super duper Костиль!
-        let html = stream ? stream.substring(0, 7) + ' style=\"position: absolute; width: 100%; height: 100%;\" ' + stream.substring(7) : null;
-        
-        return (
-            <Wrapper>
-                {!error && html &&
-                    <WebView
-                        scalesPageToFit={true}
-                        javaScriptEnabled={true}
-                        source={{ html: html }}
-                        style={styles.video}
-                    />
-                }
-            </Wrapper>
-        )
-    }
+	render() {
+		const { stream, error } = this.props;
+
+		//super duper Костиль!
+		let html = stream
+			? stream.substring(0, 7) + ' style="position: absolute; width: 100%; height: 100%;" ' + stream.substring(7)
+			: null;
+
+		return (
+			<Wrapper>
+				{!error && html && (
+					<WebView scalesPageToFit javaScriptEnabled source={{ html: html }} style={styles.video} />
+				)}
+			</Wrapper>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    video: {
-        backgroundColor: '#18142E',
-        paddingBottom: '56.25%',
-    }
-})
+	video: {
+		backgroundColor: '#18142E',
+		paddingBottom: '56.25%',
+	},
+});
 
-const mapStateToProps = (state) => {
-    const { stream, error } = state.streams;
-    return {
-        stream,
-        error,
-    }
-}
+const mapStateToProps = state => {
+	const { stream, error } = state.streams;
+	return {
+		stream,
+		error,
+	};
+};
 
-export default connect(mapStateToProps)(StreamScreen)
+export default connect(mapStateToProps)(StreamScreen);
