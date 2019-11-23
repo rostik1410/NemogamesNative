73a52314c088b36610be952704b5c3fbf33e5556
Rostyslav <ptashinskij.rostik@gmail.com>
2019-11-23 20:34:39 +0200

    back button

--- src/ScreensRouter/index.js
+++ src/ScreensRouter/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator, HeaderBackButton } from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import GamesScreen from '../components/screens/GamesScreen';
 import StreamsScreen from '../components/screens/StreamsScreen';
@@ -7,23 +7,17 @@ import StreamScreen from '../components/screens/StreamScreen';
 import Header from '../components/Header';
 
 const MainNavigator = createStackNavigator(
-    {
-        Games: { screen: GamesScreen },
-        Streams: {
-            screen: StreamsScreen,
-            navigationOptions: ({ navigation }) => ({
-                headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} />
-            }),
-        },
-        Stream: { screen: StreamScreen },
-
-    },
-    {
-        initialRouteName: 'Games',
-        defaultNavigationOptions: ({ navigation }) => ({
-            header: <Header navigation={navigation} />,
-        })
-    }
+	{
+		Games: { screen: GamesScreen },
+		Streams: { screen: StreamsScreen },
+		Stream: { screen: StreamScreen },
+	},
+	{
+		initialRouteName: 'Games',
+		defaultNavigationOptions: ({ navigation }) => ({
+			header: <Header navigation={navigation} />,
+		}),
+	}
 );
 
 export default MainNavigator;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationAction } from 'react-navigation';
+import { HeaderBackButton } from 'react-navigation-stack';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
-    static propTypes = {}
+	static propTypes = {
+		navigation: PropTypes.object,
+	};
 
-    render() {
-        return <View style={styles.header}>
-            <TouchableOpacity style={styles.logo} onPress={() => this.props.navigation.navigate('Games')}>
-                <Text style={styles.logoText}>NemoGames</Text>
-            </TouchableOpacity>
-        </View>
-    }
+	render() {
+		const { navigation } = this.props;
+
+		const isMainPage = navigation && navigation.state && navigation.state.routeName === 'Games';
+
+		return (
+			<View style={styles.header}>
+				{!isMainPage && <HeaderBackButton tintColor="#fff" onPress={() => navigation.goBack()} />}
+				<TouchableOpacity style={styles.logo} onPress={() => navigation.navigate('Games')}>
+					<Text style={styles.logoText}>NemoGames</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: 70,
-        backgroundColor: '#2C2444',
-        alignItems: 'center',
-    },
-    logo: {
-        height: 70,
-        width: '60%',
-        backgroundColor: '#33294E',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    logoText: {
-        color: 'white',
-        fontSize: 20,
-    }
-})
-
+	header: {
+		height: 70,
+		backgroundColor: '#2C2444',
+		alignItems: 'center',
+		flexDirection: 'row',
+	},
+	logo: {
+		height: 70,
+		width: '60%',
+		position: 'absolute',
+		marginHorizontal: '20%',
+		backgroundColor: '#33294E',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	logoText: {
+		color: 'white',
+		fontSize: 20,
+	},
+});
 
 export default Header;
